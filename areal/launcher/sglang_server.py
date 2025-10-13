ec2b9c9eb48bde0327c036719587ac8ee9e04021
仲青 <wht499247@antgroup.com>
2025-10-13 15:15:43 +0800

    add sglang_server log

--- areal/launcher/sglang_server.py
+++ areal/launcher/sglang_server.py
@@ -260,6 +260,8 @@ def launch_sglang_server(argv):
 
 def main(argv):
     try:
+        print("[wht debug] launch sglang server")
+        logger.info("[wht debug] Launch SGLang server")
         launch_sglang_server(argv)
     finally:
         kill_process_tree(os.getpid())
